efe4e327fa8734866efded839522cff58c087a5f
corentinm68 <corentin.margraff@gmail.com>
2017-01-11 15:39:59 +0100

    début de la création des sales

--- Labyrinth/src/labyrinth/games/dungeon/DungeonGame.java
+++ Labyrinth/src/labyrinth/games/dungeon/DungeonGame.java
@@ -3,22 +3,28 @@ package labyrinth.games.dungeon;
 import labyrinth.Direction;
 import labyrinth.Game;
 import labyrinth.Room;
-import labyrinth.games.campus.CampusGame;
+import labyrinth.games.dungeon.DungeonGame;
 
 public class DungeonGame extends Game{
 
 	public static void main(String[] args) {
-		new CampusGame().play();
+		new DungeonGame().play();
 		
     }
 	
     protected Room createRooms() {
-    	Room outside, hall, amphi, cafet, lab, office;
-
-        // création des pièces
-        outside = new Room("à l'extérieur de l'entrée principale de l'Université");
-		return outside;
+    	Room c1_in, a2, b2, c2, e2, f2, a3, c3, d3, e3, a4_out, c4, d4, e4, c5, d5, f5, d6, e6, f6, e7, e8;
     	
+    	// création des pièces
+        c1_in = new Room("sur Tatooine");
+        c2 = new Room("dans le faucon millenium");
+        a3 = new Room("en train de détruire le bouclier sur Endor");
+        a4_out = new Room("dans l'étoile de la mort");
+        
+        // initialisation des sorties des pièces
+        c1_in.setExit(Direction.SOUTH, c2);
+		
+        return c1_in;
     }
 
 }
